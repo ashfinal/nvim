037fed5da33e103ba01baa55738f01eb36fa737a
ashfinal <ashfinal@gmail.com>
2022-03-01 23:43:00 +0800

    donnot load vim runtime

--- lua/configs/lsp_servers/sumneko_lua.lua
+++ lua/configs/lsp_servers/sumneko_lua.lua
@@ -1,22 +1,16 @@
 local util = require("lspconfig.util")
 
-local nvim_rtp = vim.split(package.path, ';')
-table.insert(nvim_rtp, 'lua/?.lua')
-table.insert(nvim_rtp, 'lua/?/init.lua')
-
 return {
   root_dir = util.root_pattern(".luarc.json", ".git"),
   settings = {
     Lua = {
       runtime = {
         version = "LuaJIT",
-        path = nvim_rtp,
       },
       diagnostics = {
         globals = {"vim"},
       },
       workspace = {
-        library = vim.api.nvim_get_runtime_file("", true),
         maxPreload = 1000,
         preloadFileSize = 150,
       },
