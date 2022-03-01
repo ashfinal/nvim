c8f99def776518c77a7f9383f5f8517c54bc2495
ashfinal <ashfinal@gmail.com>
2022-03-02 01:18:55 +0800

    donnot check dirty for nomodifiable files

--- lua/utils.lua
+++ lua/utils.lua
@@ -17,16 +17,18 @@ M.bmap = function(bufnr, mode, lhs, rhs, opts)
 end
 
 M.checkdirty = function()
-  local trailing_pat = [[\s\+$]]
-  local space_pat = [[^\s\+]]
-  local tab_pat = [[^\t\+]]
-  local mixed_pat = [[^\(\t\+\s\|\s\+\t\)]]
-  local trailing_spaces = vim.fn.search(trailing_pat, "nwc")
-  local space = vim.fn.search(space_pat, "nwc")
-  local tab = vim.fn.search(tab_pat, "nwc")
-  local mixed = vim.fn.search(mixed_pat, "nwc")
-  local result = trailing_spaces > 0 or (space > 0 and tab > 0) or mixed > 0
-  if result then vim.wo.list = true else vim.wo.list = false end
+  if vim.bo.modifiable then
+    local trailing_pat = [[\s\+$]]
+    local space_pat = [[^\s\+]]
+    local tab_pat = [[^\t\+]]
+    local mixed_pat = [[^\(\t\+\s\|\s\+\t\)]]
+    local trailing_spaces = vim.fn.search(trailing_pat, "nwc")
+    local space = vim.fn.search(space_pat, "nwc")
+    local tab = vim.fn.search(tab_pat, "nwc")
+    local mixed = vim.fn.search(mixed_pat, "nwc")
+    local result = trailing_spaces > 0 or (space > 0 and tab > 0) or mixed > 0
+    if result then vim.wo.list = true else vim.wo.list = false end
+  end
 end
 
 return M
