0e791d106ae3ce02c9a7208d1f80a974422c312a
ashfinal <ashfinal@gmail.com>
2022-03-25 12:54:04 +0800

    don't map cr and bs in autopairs

--- lua/configs/autopairs.lua
+++ lua/configs/autopairs.lua
@@ -3,6 +3,9 @@ local default = {
   disable_in_macro = true,
   disable_in_visualblock = true,
   enable_check_bracket_line = false,
+  check_ts = true,
+  map_cr = false,
+  map_bs = false,
 }
 
 require('nvim-autopairs').setup(default)
