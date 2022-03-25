13f1e9702a0aac4fab53c78d8e42fd91cf286837
ashfinal <ashfinal@gmail.com>
2022-03-25 10:04:14 +0800

    clear bufferline comments

--- lua/configs/bufferline.lua
+++ lua/configs/bufferline.lua
@@ -12,10 +12,7 @@ local default = {
     end,
     left_mouse_command = "buffer %d",
     middle_mouse_command = "vertical sbuffer %d",
-    -- NOTE: this plugin is designed with this icon in mind,
-    -- and so changing this is NOT recommended, this is intended
-    -- as an escape hatch for people who cannot bear it for whatever reason
-    show_buffer_icons = true, -- disable filetype icons for buffers
+    show_buffer_icons = true,
     show_buffer_close_icons = true,
     show_close_icon = true,
     show_tab_indicators = true,
@@ -27,26 +24,11 @@ local default = {
     close_icon = "",
     left_trunc_marker = "",
     right_trunc_marker = "",
-    --- name_formatter can be used to change the buffer's label in the bufferline.
-    --- Please note some names can/will break the
-    --- bufferline so use this at your discretion knowing that it has
-    --- some limitations that will *NOT* be fixed.
-    -- name_formatter = function(buf)  -- buf contains a "name", "path" and "bufnr"
-    --   -- remove extension from markdown files for example
-    --   if buf.name:match('%.md') then
-    --     return vim.fn.fnamemodify(buf.name, ':t:r')
-    --   end
-    -- end,
     max_name_length = 18,
-    max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
+    max_prefix_length = 15,
     tab_size = 18,
     sort_by = "directory",
     diagnostics = false,
-    -- diagnostics_update_in_insert = false,
-    -- diagnostics_indicator = function(count, level, diagnostics_dict, context)
-    --   return "("..count..")"
-    -- end,
-
     offsets = {
       {
         filetype = "NvimTree",
@@ -57,27 +39,6 @@ local default = {
         text_align = "left",
       },
     },
-
-    -- NOTE: this will be called a lot so don't do any heavy processing here
-    -- custom_filter = function(buf_number, buf_numbers)
-      -- filter out filetypes you don't want to see
-      -- if vim.bo[buf_number].filetype ~= "<i-dont-want-to-see-this>" then
-      --   return true
-      -- end
-      -- filter out by buffer name
-      -- if vim.fn.bufname(buf_number) ~= "<buffer-name-I-dont-want>" then
-      --   return true
-      -- end
-      -- filter out based on arbitrary rules
-      -- e.g. filter out vim wiki buffer from tabline in your work repo
-      -- if vim.fn.getcwd() == "<work-repo>" and vim.bo[buf_number].filetype ~= "wiki" then
-      --   return true
-      -- end
-      -- filter out by it's index number in list (don't show first buffer)
-      -- if buf_numbers[1] ~= buf_number then
-      --   return true
-      -- end
-    -- end,
   }
 }
 
