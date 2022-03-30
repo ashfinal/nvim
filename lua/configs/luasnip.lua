4718ce7afda8cb73ae7f1be310ec4b6c854d0e50
ashfinal <ashfinal@gmail.com>
2022-03-30 20:57:29 +0800

    visualize luasnip active node

--- lua/configs/luasnip.lua
+++ lua/configs/luasnip.lua
@@ -1,7 +1,24 @@
 local map = require("utils").map
+local types = require("luasnip.util.types")
 
 require("luasnip.loaders.from_vscode").load()
+
 map("i", "<C-j>", "<Plug>luasnip-next-choice", { noremap = false, silent = false })
 map("s", "<C-j>", "<Plug>luasnip-next-choice", { noremap = false, silent = false })
 map("i", "<C-k>", "<Plug>luasnip-prev-choice", { noremap = false, silent = false })
 map("s", "<C-k>", "<Plug>luasnip-prev-choice", { noremap = false, silent = false })
+
+require("luasnip").config.setup({
+	ext_opts = {
+		[types.choiceNode] = {
+			active = {
+				virt_text = {{"●", "Conditional"}}
+			}
+		},
+		[types.insertNode] = {
+			active = {
+				virt_text = {{"●", "String"}}
+			}
+		}
+	},
+})
