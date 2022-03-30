b47f7ad4d1aba59d593daac1c3197ae2bdb72fce
ashfinal <ashfinal@gmail.com>
2022-03-30 20:34:05 +0800

    luasnip choice mappings

--- lua/configs/luasnip.lua
+++ lua/configs/luasnip.lua
@@ -1,3 +1,7 @@
+local map = require("utils").map
+
 require("luasnip.loaders.from_vscode").load()
-vim.api.nvim_set_keymap("i", "<C-]>", "<Plug>luasnip-next-choice", {})
-vim.api.nvim_set_keymap("s", "<C-]>", "<Plug>luasnip-next-choice", {})
+map("i", "<C-j>", "<Plug>luasnip-next-choice", { noremap = false, silent = false })
+map("s", "<C-j>", "<Plug>luasnip-next-choice", { noremap = false, silent = false })
+map("i", "<C-k>", "<Plug>luasnip-prev-choice", { noremap = false, silent = false })
+map("s", "<C-k>", "<Plug>luasnip-prev-choice", { noremap = false, silent = false })
