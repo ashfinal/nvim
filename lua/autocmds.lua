88d01e19c00d90b2456815a936f37254abc9e73b
ashfinal <ashfinal@gmail.com>
2022-04-19 03:03:25 +0800

    refactor autocmd: show invisible chars

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,9 +1,25 @@
-vim.cmd([[
-  augroup dirtyfile
-    autocmd!
-    autocmd BufReadPost,BufWritePost * lua require("utils").checkdirty()
-  augroup END
-]])
+local function check_invisible_chars()
+  if vim.bo.modifiable then
+    local trailing_pat = [[\s\+$]]
+    local space_pat = [[^\s\+]]
+    local tab_pat = [[^\t\+]]
+    local mixed_pat = [[^\(\t\+\s\|\s\+\t\)]]
+    local trailing_spaces = vim.fn.search(trailing_pat, "nwc")
+    local space = vim.fn.search(space_pat, "nwc")
+    local tab = vim.fn.search(tab_pat, "nwc")
+    local mixed = vim.fn.search(mixed_pat, "nwc")
+    local result = trailing_spaces > 0 or (space > 0 and tab > 0) or mixed > 0
+    if result then vim.wo.list = true else vim.wo.list = false end
+  end
+end
+
+vim.api.nvim_create_augroup("show_invisible_chars", { clear = true })
+vim.api.nvim_create_autocmd({ "BufReadPost", "BufWritePost" }, {
+  pattern = "*",
+  callback = check_invisible_chars,
+  group = "show_invisible_chars",
+  desc = "Check for invisible characters and find inconsistency, show them if needed",
+})
 
 local function try_stopping_lspserver(args)
   vim.lsp.for_each_buffer_client(args['buf'], function(_, client_id, bufnr)
@@ -16,7 +32,7 @@ local function try_stopping_lspserver(args)
 end
 
 vim.api.nvim_create_augroup("autostop_lspserver", { clear = true })
-vim.api.nvim_create_autocmd({"BufWipeout", "BufDelete"}, {
+vim.api.nvim_create_autocmd({ "BufWipeout", "BufDelete" }, {
   pattern = "*",
   callback = try_stopping_lspserver,
   group = "autostop_lspserver",
--- lua/utils.lua
+++ lua/utils.lua
@@ -12,19 +12,4 @@ M.bmap = function(bufnr, mode, lhs, rhs, opts)
   vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, options)
 end
 
-M.checkdirty = function()
-  if vim.bo.modifiable then
-    local trailing_pat = [[\s\+$]]
-    local space_pat = [[^\s\+]]
-    local tab_pat = [[^\t\+]]
-    local mixed_pat = [[^\(\t\+\s\|\s\+\t\)]]
-    local trailing_spaces = vim.fn.search(trailing_pat, "nwc")
-    local space = vim.fn.search(space_pat, "nwc")
-    local tab = vim.fn.search(tab_pat, "nwc")
-    local mixed = vim.fn.search(mixed_pat, "nwc")
-    local result = trailing_spaces > 0 or (space > 0 and tab > 0) or mixed > 0
-    if result then vim.wo.list = true else vim.wo.list = false end
-  end
-end
-
 return M
