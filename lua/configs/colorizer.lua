2d63a4defdaf4a042abb8427e3a205db3745f9d1
ashfinal <ashfinal@gmail.com>
2022-10-30 22:13:58 +0800

    update colorizer config

--- lua/configs/colorizer.lua
+++ lua/configs/colorizer.lua
@@ -3,21 +3,21 @@ local default = {
     "vim",
     "html",
     "css",
-    "scss",
+    "sass",
     "javascript",
     "typescript",
   },
-  options = {
+  user_default_options = {
     RGB = true, -- #RGB hex codes
     RRGGBB = true, -- #RRGGBB hex codes
-    names = false, -- "Name" codes like Blue
-    RRGGBBAA = false, -- #RRGGBBAA hex codes
-    rgb_fn = false, -- CSS rgb() and rgba() functions
-    hsl_fn = false, -- CSS hsl() and hsla() functions
-    css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-    css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+    names = true, -- "Name" codes like Blue
+    RRGGBBAA = true, -- #RRGGBBAA hex codes
+    rgb_fn = true, -- CSS rgb() and rgba() functions
+    hsl_fn = true, -- CSS hsl() and hsla() functions
+    tailwind = true,  -- Enable tailwind colors
+    sass = { enable = true, parsers = { css }, }, -- Enable sass colors
     mode = "virtualtext", -- Set the display mode.
   }
 }
 
-require("colorizer").setup(default["filetypes"], default["options"])
+require("colorizer").setup(default)
