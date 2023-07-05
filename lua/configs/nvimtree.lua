83e1b3edb83e33c39364d637d57efe8647130928
ashfinal <ashfinal@gmail.com>
2023-07-05 15:08:50 +0800

    override nvim-tree default mappings

--- lua/configs/nvimtree.lua
+++ lua/configs/nvimtree.lua
@@ -1,3 +1,17 @@
+local function on_attach(bufnr)
+  local api = require("nvim-tree.api")
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  api.config.mappings.default_on_attach(bufnr)
+  vim.keymap.del("n", "<BS>", { buffer = bufnr })
+  vim.keymap.del("n", "g?", { buffer = bufnr })
+  vim.keymap.set("n", "w", api.node.navigate.parent_close, opts("Close Directory"))
+  vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
+end
+
 local default = {
   disable_netrw = true,
   hijack_netrw = true,
@@ -40,8 +54,9 @@ local default = {
       },
     },
   },
+  on_attach = on_attach,
 }
 
 require("nvim-tree").setup(default)
 
-vim.keymap.set("n", "<Leader>e", function() return require('nvim-tree.api').tree.toggle() end, { desc = "Toggle file explorer" })
+vim.keymap.set("n", "<Leader>e", function() return require("nvim-tree.api").tree.toggle() end, { desc = "Toggle file explorer" })
