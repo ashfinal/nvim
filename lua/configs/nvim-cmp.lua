a91a1a62ff647130e80e450e85668398eee4b889
ashfinal <ashfinal@gmail.com>
2023-07-11 16:48:58 +0800

    enable completion in a comment

--- lua/configs/nvim-cmp.lua
+++ lua/configs/nvim-cmp.lua
@@ -36,15 +36,6 @@ local default = {
     if vim.tbl_contains({ "TelescopePrompt" }, vim.o.ft) then
       return false
     end
-    -- disable completion in comments
-    local context = require("cmp.config.context")
-    -- keep command mode completion enabled when cursor is in a comment
-    if vim.api.nvim_get_mode().mode == "c" then
-      return true
-    else
-        return not context.in_treesitter_capture("comment")
-          and not context.in_syntax_group("Comment")
-    end
   end,
 
   snippet = {
