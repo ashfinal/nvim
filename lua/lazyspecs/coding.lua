7f7659110ffbf605ae30a0b8740ccdbb01c8af92
ashfinal <ashfinal@gmail.com>
2023-08-08 14:27:03 +0800

    attach cmp source whenever copilot attaches

--- lua/lazyspecs/coding.lua
+++ lua/lazyspecs/coding.lua
@@ -254,6 +254,13 @@ return {
         config = function(_, opts)
           local copilot_cmp = require("copilot_cmp")
           copilot_cmp.setup(opts)
+          -- attach cmp source whenever copilot attaches
+          -- fixes lazy-loading issues with the copilot cmp source
+          require("utils").on_attach(function(client)
+            if client.name == "copilot" then
+              copilot_cmp._on_insert_enter({})
+            end
+          end)
         end,
       },
     },
