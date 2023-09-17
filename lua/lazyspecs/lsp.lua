c8f20844fca9ba6203920e62d4a6ec4d3bcd8df2
ashfinal <ashfinal@gmail.com>
2023-09-17 21:17:48 +0800

    add rust_analyzer lspconfig

--- lua/lazyspecs/lsp.lua
+++ lua/lazyspecs/lsp.lua
@@ -66,6 +66,31 @@ return {
               clangdFileStatus = true,
             },
           },
+          rust_analyzer = {
+            settings = {
+              ["rust-analyzer"] = {
+                cargo = {
+                  allFeatures = true,
+                  loadOutDirsFromCheck = true,
+                  runBuildScripts = true,
+                },
+                -- Add clippy lints for Rust.
+                checkOnSave = {
+                  allFeatures = true,
+                  command = "clippy",
+                  extraArgs = { "--no-deps" },
+                },
+                procMacro = {
+                  enable = true,
+                  ignored = {
+                    ["async-trait"] = { "async_trait" },
+                    ["napi-derive"] = { "napi" },
+                    ["async-recursion"] = { "async_recursion" },
+                  },
+                },
+              },
+            },
+          },
           pyright = {},
           taplo = {},
           yamlls = {
