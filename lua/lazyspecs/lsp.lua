c4cdf6aacbbb674e8c5b9ddfa61fe0b16f35ff65
ashfinal <ashfinal@gmail.com>
2023-09-17 21:12:39 +0800

    add clangd lspconfig

--- lua/lazyspecs/lsp.lua
+++ lua/lazyspecs/lsp.lua
@@ -28,7 +28,44 @@ return {
           cssls = {},
           html = {},
           ltex = { autostart = false },
-          sourcekit = {},
+          sourcekit = {
+            filetypes = { "swift" },
+            root_dir = require("lspconfig.util").root_pattern("Package.swift")
+          },
+          clangd = {
+            root_dir = require("lspconfig.util").root_pattern(
+              "Makefile",
+              "configure.ac",
+              "configure.in",
+              "config.h.in",
+              "meson.build",
+              "meson_options.txt",
+              "build.ninja",
+              "compile_commands.json",
+              "compile_flags.txt",
+              ".clangd",
+              ".clang-tidy",
+              ".clang-format",
+              ".git"
+            ),
+            capabilities = {
+              offsetEncoding = { "utf-16" },
+            },
+            cmd = {
+              "clangd",
+              "--background-index",
+              "--clang-tidy",
+              "--header-insertion=iwyu",
+              "--completion-style=detailed",
+              "--function-arg-placeholders",
+              "--fallback-style=llvm",
+            },
+            init_options = {
+              usePlaceholders = true,
+              completeUnimported = true,
+              clangdFileStatus = true,
+            },
+          },
           pyright = {},
           taplo = {},
           yamlls = {
