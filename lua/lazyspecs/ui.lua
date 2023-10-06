a0beb4a1565bd89f4f986734f10908a02ffb6793
ashfinal <ashfinal@gmail.com>
2023-10-06 13:21:35 +0800

    don't show bufferline.nvim's tab close icon

--- lua/lazyspecs/ui.lua
+++ lua/lazyspecs/ui.lua
@@ -23,6 +23,7 @@ return {
           buffer_close_icon = "󰅖",
           modified_icon = "",
           close_icon = "󰅚",
+          show_close_icon = false,
           custom_filter = function(buf_number)
             if vim.bo[buf_number].filetype ~= "qf" then
               return true
