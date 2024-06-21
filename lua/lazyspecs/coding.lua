04d937830ee58d5488f90564d7249eaace4f4557
ashfinal <ashfinal@gmail.com>
2024-06-21 08:48:34 +0800

    add plugin lazydev.nvim

--- lua/lazyspecs/coding.lua
+++ lua/lazyspecs/coding.lua
@@ -319,4 +319,23 @@ return {
       end,
     },
   },
+  {
+    "folke/lazydev.nvim",
+    ft = "lua",
+    cmd = "LazyDev",
+    opts = {
+      library = {
+        { path = "luvit-meta/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+  -- Manage libuv types with lazy. Plugin will never be loaded
+  { "Bilal2453/luvit-meta", lazy = true },
+  -- Add lazydev source to cmp
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      table.insert(opts.sources, { name = "lazydev", group_index = 0 })
+    end,
+  },
 }
