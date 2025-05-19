48e8a2e255fa879836bd25466fe363974941b937
ashfinal <ashfinal@gmail.com>
2025-05-20 04:51:17 +0800

    disable hover highlights

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -26,3 +26,10 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   group = vim.api.nvim_create_augroup("highlight_yanked_text", {}),
   desc = "Briefly highlight yanked text",
 })
+
+-- disable hover highlights
+vim.api.nvim_create_autocmd("ColorScheme", {
+  callback = function()
+    vim.api.nvim_set_hl(0, "LspReferenceTarget", {})
+  end,
+})
