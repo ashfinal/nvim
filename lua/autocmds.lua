ac3fd49dac41998e46273ffbe5f4530b3333692d
ashfinal <ashfinal@gmail.com>
2025-11-16 13:40:22 +0800

    reveal_invisible_chars may cause nvim to freeze

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,18 +1,3 @@
-local function reveal_invisible_chars()
-  if vim.bo.modifiable then
-    local search_pat = [[ \+$\|^ \+\_.\+\_^\t\+\|^\t\+\_.\+\_^ \+\|^\(\t\+ \| \+\t\)]]
-    local match_result = vim.fn.search(search_pat, "nwc") > 0
-    vim.wo.list = match_result
-  end
-end
-
-vim.api.nvim_create_autocmd({ "BufReadPost", "BufWritePost" }, {
-  pattern = "*",
-  callback = reveal_invisible_chars,
-  group = vim.api.nvim_create_augroup("reveal_invisible_chars", {}),
-  desc = "Check for invisible characters and find inconsistency, reveal them if necessary",
-})
-
 vim.api.nvim_create_autocmd("TextYankPost", {
   pattern = "*",
   callback = function() vim.hl.on_yank({ on_visual = false }) end,
